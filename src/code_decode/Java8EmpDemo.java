8b0a6154318a80db766a31adc019b8b767817bf4
CORP\pkuma834 <151085priti@gmail.com>
2022-06-05 17:34:13 +0800

    2nd code

--- src/code_decode/Java8EmpDemo.java
+++ src/code_decode/Java8EmpDemo.java
@@ -14,13 +14,21 @@ public class Java8EmpDemo {
 		empList.add(new Employee(5, 2500));
 		empList.add(new Employee(6, 3000));
 		empList.add(new Employee(7, 3500));
-
+		
+       //sort employee based on their salaries in descending order
 		List<Employee> empSortList = empList.stream().sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()))
 				.collect(Collectors.toList());
-		System.out.println(empSortList);
-		
-		// fetch Top 3 salaried employee
-		
+		System.out.println("sort employee based on their salaries in descending order : " + empSortList);
+		System.out.println("\n");
+		// fetch Top 3 salaried employee using limit()
+		List<Employee> empSortList1 = empList.stream().sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()))
+				.limit(3).collect(Collectors.toList());
+		System.out.println("fetch Top 3 salaried employee : " + empSortList1);
+		System.out.println("\n");
+		// Fetch all employee having salary less than 3rd highest salary using skip()?
+		List<Employee> empSortList2 = empList.stream().sorted((o1, o2) -> (int) (o2.getSalary() - o1.getSalary()))
+				.skip(3).collect(Collectors.toList());
+		System.out.println("Fetch all employee having salary less than 3rd highest salary : " + empSortList2);
 	}
 
 }
